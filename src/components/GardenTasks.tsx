8ab54fce5f62c5d2f95bedb49896ca6fa73f8bd2
Anders Høgh <ahha15@student.aau.dk>
2020-04-22 13:45:30 +0200

    Reformatted garden

--- src/components/GardenTasks.tsx
+++ src/components/GardenTasks.tsx
@@ -1,49 +1,63 @@
-import React from 'react';
-import { IonCol, IonRow, IonCard, IonCardContent } from '@ionic/react';
+import React from "react";
+import {
+  IonCol,
+  IonRow,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonIcon,
+  IonCardSubtitle,
+  IonText,
+} from "@ionic/react";
+import { leafOutline } from "ionicons/icons";
 
 interface GardenTasksProps {
   tasks: firebase.firestore.DocumentData[];
 }
 
-const GardenTasks: React.FC<GardenTasksProps> = props => {
+const GardenTasks: React.FC<GardenTasksProps> = (props) => {
   const { tasks } = props;
-  
+
   let tasksDoing: string[];
   tasksDoing = [];
-  tasks.forEach(task => {
+  tasks.forEach((task) => {
     if (!tasksDoing.includes(task.taskTemplateId)) {
       tasksDoing.push(task.taskTemplateId);
     }
   });
 
-  let taskString = '';
+  let taskString = "";
   tasksDoing.forEach((task, index) => {
     if (index < tasksDoing.length - 2) {
-      taskString += task + ', ';
+      taskString += task + ", ";
     } else if (index === tasksDoing.length - 2) {
-      taskString += task + ' and ';
+      taskString += task + " and ";
     } else {
       taskString += task;
     }
-  })
+  });
 
   return (
     <>
-      {tasksDoing.length !== 0 ? 
+      {tasksDoing.length !== 0 ? (
         <IonRow>
           <IonCol>
             <IonCard>
+              <IonCardHeader className="card-div">
+                <IonIcon className="icon" icon={leafOutline}></IonIcon>
+              </IonCardHeader>
+              <IonCardContent className="card-div">
+                <IonCardSubtitle>People are currently</IonCardSubtitle>
+              </IonCardContent>
               <IonCardContent>
-                <span>
-                  Right now, people are {taskString} in the garden.
-                </span>
+                <IonText>{taskString}</IonText>
               </IonCardContent>
             </IonCard>
           </IonCol>
         </IonRow>
-      : null }
+      ) : null}
     </>
   );
-}
+};
 
 export default GardenTasks;
